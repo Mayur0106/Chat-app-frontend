b3d8ea5b34cc262ae1e96a03ed5e482179511966
Mayur Shinde <97580421+Mayur0106@users.noreply.github.com>
2024-05-01 23:25:21 +0530

    Update appContext.js

--- src/context/appContext.js
+++ src/context/appContext.js
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 import React from "react";
-const SOCKET_URL = "https://chat-app-api-kwu7.onrender.com";
+const SOCKET_URL = `${process.env.REACT_APP_CHAT_APP_SERVER_URL}`;
 export const socket = io(SOCKET_URL);
 
 export const AppContext = React.createContext();
